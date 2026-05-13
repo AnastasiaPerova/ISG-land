77a34e61f336aafb054e7e6d24453d5f899ee7b8
Anastasia Perova <anastasiaperova92@gmail.com>
2026-05-13 21:48:26 +0300

    Update

--- template-parts/site-logo.php
+++ template-parts/site-logo.php
@@ -8,7 +8,7 @@
 $logo_url = isg_acf_image_url('header_logo', 'option', isg_asset_uri('img/logo_footer.svg'));
 $home_hero_url = isg_anchor_url('#isg-hero', '#isg-hero');
 ?>
-<div class="isg-site-logo" data-isg-section-nav>
+<!-- <div class="isg-site-logo" data-isg-section-nav>
 	<a class="isg-hero__logo-link" href="<?php echo esc_url($home_hero_url); ?>" aria-label="<?php esc_attr_e('ISG - Home', 'isg'); ?>">
 		<img
 			class="isg-hero__logo-img"
@@ -19,4 +19,4 @@ $home_hero_url = isg_anchor_url('#isg-hero', '#isg-hero');
 			decoding="async"
 		/>
 	</a>
-</div>
+</div> -->
