b41254a8204be832f8b117f92c1c16e2a919df69
Anastasia Perova <anastasiaperova92@gmail.com>
2026-05-13 19:43:49 +0300

    Update

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -35,31 +35,6 @@ function isg_theme_setup(): void {
 }
 add_action('after_setup_theme', 'isg_theme_setup');
 
-function isg_allow_svg_uploads(array $mimes): array {
-	if (current_user_can('upload_files')) {
-		$mimes['svg'] = 'image/svg+xml';
-	}
-
-	return $mimes;
-}
-add_filter('upload_mimes', 'isg_allow_svg_uploads');
-
-function isg_check_svg_filetype(array $data, string $file, string $filename, array $mimes): array {
-	unset($file, $mimes);
-
-	if (!current_user_can('upload_files')) {
-		return $data;
-	}
-
-	if (strtolower(pathinfo($filename, PATHINFO_EXTENSION)) === 'svg') {
-		$data['ext']  = 'svg';
-		$data['type'] = 'image/svg+xml';
-	}
-
-	return $data;
-}
-add_filter('wp_check_filetype_and_ext', 'isg_check_svg_filetype', 10, 4);
-
 function isg_body_classes(array $classes): array {
 	$classes[] = 'isg-wp-theme';
 	$classes[] = 'isg-preloader-active';
