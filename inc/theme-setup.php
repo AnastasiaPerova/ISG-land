4c74f12aa3b39b9128672998b2e5cb887bccd845
Anastasia Perova <anastasiaperova92@gmail.com>
2026-05-13 19:39:34 +0300

    Update

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -35,61 +35,30 @@ function isg_theme_setup(): void {
 }
 add_action('after_setup_theme', 'isg_theme_setup');
 
-function isg_can_upload_svg(): bool {
-	return current_user_can('upload_files');
-}
-
 function isg_allow_svg_uploads(array $mimes): array {
-	if (!isg_can_upload_svg()) {
-		return $mimes;
+	if (current_user_can('upload_files')) {
+		$mimes['svg'] = 'image/svg+xml';
 	}
-
-	$mimes['svg'] = 'image/svg+xml';
 
 	return $mimes;
 }
 add_filter('upload_mimes', 'isg_allow_svg_uploads');
 
 function isg_check_svg_filetype(array $data, string $file, string $filename, array $mimes): array {
-	unset($mimes);
+	unset($file, $mimes);
 
-	if (!isg_can_upload_svg() || strtolower(pathinfo($filename, PATHINFO_EXTENSION)) !== 'svg') {
+	if (!current_user_can('upload_files')) {
 		return $data;
 	}
 
-	$contents = is_readable($file) ? file_get_contents($file) : false;
-	if (!is_string($contents) || !preg_match('/<svg[\s>]/i', $contents)) {
-		return $data;
+	if (strtolower(pathinfo($filename, PATHINFO_EXTENSION)) === 'svg') {
+		$data['ext']  = 'svg';
+		$data['type'] = 'image/svg+xml';
 	}
-
-	$data['ext']             = 'svg';
-	$data['type']            = 'image/svg+xml';
-	$data['proper_filename'] = false;
 
 	return $data;
 }
 add_filter('wp_check_filetype_and_ext', 'isg_check_svg_filetype', 10, 4);
-
-function isg_disable_upload_image_processing($threshold = false) {
-	unset($threshold);
-	return false;
-}
-add_filter('big_image_size_threshold', 'isg_disable_upload_image_processing');
-
-function isg_disable_upload_image_subsizes(array $sizes): array {
-	return array();
-}
-add_filter('intermediate_image_sizes_advanced', 'isg_disable_upload_image_subsizes');
-
-function isg_disable_upload_displayable_image_processing($result, $path = '') {
-	$extension = strtolower(pathinfo((string) $path, PATHINFO_EXTENSION));
-	if (in_array($extension, array('jpg', 'jpeg', 'png', 'webp', 'gif', 'avif', 'heic'), true)) {
-		return false;
-	}
-
-	return $result;
-}
-add_filter('file_is_displayable_image', 'isg_disable_upload_displayable_image_processing', 10, 2);
 
 function isg_body_classes(array $classes): array {
 	$classes[] = 'isg-wp-theme';
